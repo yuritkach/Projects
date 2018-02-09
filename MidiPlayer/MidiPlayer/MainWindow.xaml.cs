4c0f9f6dbdbd2e94c80daf9ed474a88d6b4983cf
yuritkach <yuritkach@gmail.com>
2018-02-09 16:46:57 +0200

    Первичная заливка

---
+++ MidiPlayer/MidiPlayer/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Player player;
+        Settings settings;
+
+        protected void init() {
+            player = new Player();
+            settings = new Settings();
+        }
+        public MainWindow()
+        {
+            InitializeComponent();
+            init();
+        }
+
+        private void playerbutton_Click(object sender, RoutedEventArgs e)
+        {
+            mainframe.Content = player;
+        }
+
+        private void settingsbutton_Click(object sender, RoutedEventArgs e)
+        {
+            mainframe.Content = settings;
+        }
+    }
+}
---
+++ MidiPlayer/MidiPlayer/Player.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication4
+{
+    /// <summary>
+    /// Логика взаимодействия для Player.xaml
+    /// </summary>
+    public partial class Player : Page
+    {
+        Stuff stuff;
+        public Player()
+        {
+            InitializeComponent();
+            stuff = new Stuff();
+            stuff.Draw(StuffCanvas);
+        }
+    }
+
+
+    public class Stuff{
+        public enum StuffType { stBass,stTreble,stBoth};
+        protected Canvas cnv;
+        protected StuffType stuffType;
+
+        public Stuff() {
+            stuffType = StuffType.stBoth;
+        }
+
+        protected void DrawLines() {
+            if (stuffType == StuffType.stBoth) {
+                var br = new SolidColorBrush();
+                br.Color = Color.FromRgb(0,0,0);
+                for (int i = 0; i < 5; i++) {
+                    Line ln = new Line();
+                    ln.X1 = 10;
+                    ln.Y1 = 10*i;
+                    ln.X2 = 100;
+                    ln.Y2 = 10 * i;
+                    ln.Stroke = br;
+
+                    cnv.Children.Add(ln);
+                }
+                
+            }
+        }
+        protected void DrawBars() { }
+        protected void DrawNotes() { }
+        protected void DrawSign() { }
+
+
+        public void Draw(Canvas _cnv) {
+            cnv = _cnv;
+            DrawLines();
+            DrawBars();
+            DrawNotes();
+            DrawSign();
+        }
+
+    }
+
+}
